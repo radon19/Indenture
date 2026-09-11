aa5e9cab2a27afd2a85bb98cf1fac3bdcfbb7963
Kedar Singh Naikane <kedarsinghnaikane@gmail.com>
2026-09-11 14:39:38 +0530

    modified: providers

--- providers/config.ts
+++ providers/config.ts
@@ -3,11 +3,19 @@ import {
  sepolia,
  creditCoin3Testnet
 } from 'wagmi/chains';
+import { http, fallback } from 'wagmi';
 
 const config = getDefaultConfig({
   appName: 'Indenture',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID??"",
   chains: [sepolia, creditCoin3Testnet],
+  transports: {
+    [sepolia.id]: http(),
+    [creditCoin3Testnet.id]: fallback([
+      http(process.env.NEXT_PUBLIC_CREDITCOIN_RPC_URL),
+      http(),
+    ]),
+  },
   ssr: true, 
 });
 
